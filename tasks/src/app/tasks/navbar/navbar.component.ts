a47112d0619b884d04a2223ca3500367790b3d1c
Alexandra Bledea <bledea_alexandra@yahoo.com>
2025-02-06 22:28:54 +0200

    Added notification bar

--- tasks/src/app/tasks/navbar/navbar.component.ts
+++ tasks/src/app/tasks/navbar/navbar.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent  {
 
+  notifications: { message: string }[] = [{message: 'notif'}, {message: 'notif'}, {message: 'notif'}, {message: 'notif'}]; // Notifications array
+  showNotifications: boolean = false;
 
   constructor(private router: Router) {
+    this.loadNotifications();
+  }
+
+  toggleNotifications(): void {
+    this.showNotifications = !this.showNotifications;
+  }
+
+  loadNotifications(): void {
+    // Mock notifications
+    this.notifications = [
+      { message: 'Task "Test DB2" has been assigned to you' },
+      { message: 'Task "Test Email" is due soon' },
+    ];
   }
 
   goToTasks() {
