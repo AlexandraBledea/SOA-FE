6336717c9a3efe7c75365f23c90c5a3bb0e072a0
Alexandra Bledea <bledea_alexandra@yahoo.com>
2025-02-07 16:17:12 +0200

    Removed stomp service

--- tasks/src/app/tasks/navbar/navbar.component.ts
+++ tasks/src/app/tasks/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {NgForOf, NgIf} from '@angular/common';
-import {StompService} from '../../service/stomp.service';
 
 @Component({
   selector: 'app-navbar',
@@ -13,36 +12,9 @@ import {StompService} from '../../service/stomp.service';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
-  // notifications: { message: string }[] = [{message: 'notif'}, {message: 'notif'}, {message: 'notif'}, {message: 'notif'}]; // Notifications array
-  // showNotifications: boolean = false;
-
-  // ngOnInit() {
-  //   // Initialize WebSocket only after component is fully loaded
-  //   this.stompSerice.initialize();
-  //
-  //   // Subscribe to WebSocket messages
-  //   this.stompSerice.messages$.subscribe(() => {
-  //     console.log("s-a emis notif");
-  //     this.notifications = [...this.notifications, {message: 'notif emisa noua'}];
-  //   });
-  // }
-
-  constructor(private router: Router, private stompSerice: StompService) {
-    // this.loadNotifications();
+  
+  constructor(private router: Router) {
   }
-  //
-  // toggleNotifications(): void {
-  //   this.showNotifications = !this.showNotifications;
-  // }
-  //
-  // loadNotifications(): void {
-  //   // Mock notifications
-  //   this.notifications = [
-  //     { message: 'Task "Test DB2" has been assigned to you' },
-  //     { message: 'Task "Test Email" is due soon' },
-  //   ];
-  // }
 
   goToTasks() {
     this.router.navigate(['/main/tasks']);
